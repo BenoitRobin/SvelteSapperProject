76e194573f1edd36ff6b61f3fdf5b1813265f4f1
BenoitRobin <benoitrobin@me.com>
2020-10-22 13:58:19 +0200

    add MeetUps components with hard state

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,40 @@
 <script>
     import Header from './UI/Header.svelte';
+    import MeetUpGrid from './Meetups/MeetUpGrid.svelte';
 
+    const meetups= [
+        {
+            id: 'm1',
+            title: 'Coding Bootcamp',
+            subtitle: 'Learn to code in 2 hours',
+            description: 'In this meetup, we will have some experts that teach you how to use Svelte to build your SPA',
+            imageUrl: 'https://www.numerama.com/content/uploads/2016/11/open-data-code.jpg',
+            address: '27 Nerd Road, 32523 New York',
+            contactEmail: 'code@test.com'
+        },
+        {
+            id: 'm2',
+            title: 'Swim together',
+            subtitle: 'Let\'s go for swimming',
+            description: 'We will simply swim some rounds',
+            imageUrl: 'https://res.cloudinary.com/sagacity/image/upload/c_crop,h_731,w_1097,x_0,y_0/c_limit,dpr_auto,f_auto,fl_lossy,q_80,w_1200/Superior_Pools_Residential_Restoration_sby0yt.jpg',
+            address: '44, rue Bezout 75014 Paris',
+            contactEmail: 'swim@test.com'
+        }
+    ]
 </script>
 
+<style>
+    main {
+        padding-top: 5rem;
+    }
+</style>
+
 <Header />
+
+<main>
+    <MeetUpGrid {meetups}/>
+</main>
+
 
 
---
+++ src/Meetups/MeetUpGrid.svelte
@@ -0,0 +1,37 @@
+<script>
+    import MeetUpItem from './MeetUpItem.svelte';
+
+    export let meetups;
+</script>
+
+<style>
+    
+section {
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 1rem;
+}
+
+@media (min-width: 768px) {
+    section {
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+
+</style>
+
+<section id="meetups">
+    
+    {#each meetups as meetUp}
+        <MeetUpItem
+            title={meetUp.title}
+            subtitle={meetUp.subtitle}
+            description={meetUp.description}
+            imageUrl={meetUp.imageUrl}
+            contactEmail={meetUp.email}
+            address={meetUp.address}    
+        />
+    {/each}
+    
+    </section>
