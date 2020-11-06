752da4525a737ca10e18079ba176075daf28b354
BenoitRobin <benoitrobin@me.com>
2020-11-06 13:12:13 +0100

    add filter component

--- src/Meetups/MeetUpGrid.svelte
+++ src/Meetups/MeetUpGrid.svelte
@@ -1,26 +1,43 @@
 <script>
   import MeetUpItem from "./MeetUpItem.svelte";
+  import MeetupFilter from "./MeetupFilter.svelte";
+  import App from "../App.svelte";
 
   export let meetups;
+
+  let favsOnly = false;
+
+  $: filterMeetups = favsOnly ? meetups.filter((m) => m.isFavorite) : meetups;
+
+  function setFilter(event) {
+    favsOnly = event.detail === 1;
+  }
 </script>
 
 <style>
-  section {
+  #meetups {
     width: 100%;
     display: grid;
     grid-template-columns: 1fr;
     grid-gap: 1rem;
   }
+  #meetup-controls {
+    margin: 1rem;
+  }
 
   @media (min-width: 768px) {
-    section {
+    #meetups {
       grid-template-columns: repeat(2, 1fr);
     }
   }
 </style>
 
+<section id="meetup-controls">
+  <MeetupFilter on:select={setFilter} />
+</section>
+
 <section id="meetups">
-  {#each meetups as meetUp}
+  {#each filterMeetups as meetUp}
     <MeetUpItem
       id={meetUp.id}
       title={meetUp.title}
